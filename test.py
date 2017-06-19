a5130921cfe6927568a253eb96d16460dd3805e4
Pablo Castellano <pablo@anche.no>
2017-06-19 17:41:40 +0200

    Fix test.py

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python
 import sys
 sys.path.append("refine.py")
 import refine
 
 r = refine.Refine()
-p = r.new_project("dates.txt")
+p = r.new_project("dates.csv")
 p.apply_operations("operations.json")
 print p.export_rows()
 p.delete_project()
